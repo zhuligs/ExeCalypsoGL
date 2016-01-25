039a4594034ae6432f6a64e7b64634c279371651
Li Zhu(MacbookPro) <zhulipresent@gmail.com>
2016-01-25 13:33:54 -0500

    fix the check job bug

--- cgl.py
+++ cgl.py
@@ -1,7 +1,29 @@
 #!/usr/bin/python -u
 
+#******************************************************************************
 # Copyright (C) 2015 Li Zhu
 # All rights reserved.
+#
+# cgl.py
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+# ****************************************************************************/
 
 import os
 import time
@@ -67,7 +89,7 @@ def checkjob(idpool):
             time.sleep(2)
     kbuff = jbuff.split('\n')
     remids = []
-    if len(kbuff[6].strip()) > 0:
+    if len(kbuff) > 5 and len(kbuff[6].strip()) > 0:
         for x in kbuff[6:-1]:
             remids.append(int(x.split()[0]))
     else:
