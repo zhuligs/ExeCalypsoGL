6396137751327cd7341fb524799f9a3ac14f56a3
Li Zhu(MacbookPro) <zhulipresent@gmail.com>
2016-01-28 14:12:02 -0500

    cgll

--- cgll.py
+++ cgll.py
@@ -1,13 +1,38 @@
 #!/usr/bin/python -u
 # encoding: utf-8
 
+# *****************************************************************************
 # Copyright (C) 2016 Li Zhu
-# All rights reversed.
+# All rights reserved.
+#
+# cgll.py
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+# *****************************************************************************
 
 import os
 import time
 import cPickle as pick
 import glob
+import subprocess
+
+# optimized for DOD cluster
 
 def pushlocal(istep, npop, bdir):
     """push geometry opt local
@@ -31,4 +56,142 @@ def pushlocal(istep, npop, bdir):
     pick.dump(idpool, f)
     f.close()
     return idpool
+
+
+def checkjob(idpool):
+
+    while True:
+        try:
+            jbuff = subprocess.check_output(["qstat", "-u", "zhuli"])
+            break
+        except:
+            time.sleep(2)
+
+    if len(jbuff.strip()) > 0:
+        kbuff = jbuff.strip().split('\n')
+        reminds = []
+        try:
+            for x in kbuff[2:]:
+                reminds.append(x.split()[0])
+        except:
+            return True
+    else:
+        return True
+    finished = True
+    for id in idpool:
+        if id in reminds:
+            finished = False
+    return finished
+
+
+def pullocal(bdir, npop, istep):
+    for i in range(npop):
+        ip = str(i + 1)
+        rdir = bdir + '/' + ip
+        while True:
+            try:
+                subprocess.call(["cp", rdir + "/CONTCAR", "CONTCAR_" + ip])
+                subprocess.call(["cp", rdir + "/OUTCAR", "OUTCAR_" + ip])
+                break
+            except:
+                time.sleep(2)
+        if checkcontcar('CONTCAR_' + ip):
+            subprocess.call(["cp", "POSCAR_" + ip, "CONTCAR_" + ip])
+    subprocess.call(["mkdir", "-p", "data" + str(istep)])
+    subprocess.call(["cp", "POSCAR_*", "OUTCAR_*", "CONTCAR_*",
+                     "data" + str(istep)])
+
+
+def checkcontcar(contcar):
+    buff = subprocess.check_output(["du", contcar])
+    jbuff = int(buff.split()[0])
+    if jbuff == 0:
+        return True
+    else:
+        return False
+
+
+def newjob(bdir, kstep, maxstep, npop):
+    for istep in range(kstep, maxstep + 1):
+        print 'ISTEP', istep
+        subprocess.call(["./calypso.x", ">>CALYPSO.STDOUT"])
+        cglstatus = 0
+        dumpgcl(cglstatus)
+        idpool = pushjob(istep, npop, bdir)
+        print 'idpool', idpool
+        cglstatus = 1
+        dumpgcl(cglstatus)
+        finished = False
+        while not finished:
+            if checkjob(idpool):
+                pullocal(bdir, npop, istep)
+                finished = True
+                print 'OPT FINISHED', istep
+            print 'OPT NOT YET FINISH', istep
+            time.sleep(30)
+        cglstatus = 2
+        dumpgcl(cglstatus)
+
+
+def dumpgcl(cglstatus):
+    f = open('cgls.dat', 'w')
+    pick.dump(cglstatus, f)
+    f.close()
+
+
+def loadgcl():
+    f = open('cgls.dat')
+    cglstatus = pick.load(f)
+    f.close()
+    return cglstatus
+
+
+def readinput():
+    finput = 'input.dat'
+    indata = {}
+    f = open(finput, 'r')
+    for line in f:
+        if '=' in line:
+            if line.strip()[0] != '#':
+                litem = line.split('=')
+                indata[litem[0].strip().lower()] = litem[1].strip()
+    f.close()
+
+    npop = int(indata['popsize'])
+    try:
+        systemname = indata['systemname']
+    except:
+        systemname = 'CALY'
+
+    maxstep = int(indata['maxstep'])
+
+    return(systemname, npop, maxstep)
+
+
+def cgl():
+    (systemname, npop, maxstep) = readinput()
+    bdir = 'CGL/' + systemname
+    (restart, kstep) = check_status()
+    if restart:
+        print 'RESTART JOB'
+        restartjob(bdir, kstep, maxstep, npop)
+    else:
+        print 'NEW JOB'
+        newjob(bdir, kstep, maxstep, npop)
+    return 0
+
+
+if __name__ == "__main__":
+    # print checkjob([996705])
+    # print checkjob([234])
+    cgl()
+
+
+
+
+
+
+
+
+
 
